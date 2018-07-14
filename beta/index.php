b301432456cb6d1b739e0d1db87f75329ecadfac
Vishnu Poothery <vishnupoothery@gmail.com>
2018-07-14 21:41:15 +0530

    beta started

---
+++ beta/index.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"></meta>
+
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+    ></meta>
+
+    <title>Hello Bulma!</title>
+
+    <link
+        rel="stylesheet"
+        href="css/bulma.min.css"
+    ></link>
+
+    <script
+        defer="defer"
+        src="https://use.fontawesome.com/releases/v5.1.0/js/all.js"
+    ></script>
+</head>
+
+<body>
+    <section class="hero is-primary is-medium">
+        <div class="hero-body">
+            <div class="container">
+                <h1 class="title is-1">Coding Quotes</h1>
+
+                <h2 class="subtitle">Like your favorites</h2>
+            </div>
+        </div>
+    </section>
+
+    <section class="section">
+        <div class="container">
+            <div class="columns">
+                <div class="column">
+                    <div class="card">
+                        <div class="card-content">
+                            <h2 class="title">"Quote"</h2>
+
+                            <h3 class="subtitle">Programmer</h3>
+                        </div>
+
+                        <footer class="card-footer">
+                            <span class="card-footer-item">
+                                <a
+                                    href="#"
+                                    class="button is-success"
+                                >
+                                    <i class="fa fa-thumbs-o-up"></i>
+                                </a>
+                            </span>
+
+                            <span class="card-footer-item">
+                                <a
+                                    href="#"
+                                    class="button is-danger"
+                                >
+                                    <i class="fa fa-thumbs-o-down"></i>
+                                </a>
+                            </span>
+
+                            <span class="card-footer-item">
+                                <a
+                                    href="#"
+                                    class="button is-info"
+                                >
+                                    <i class="fa fa-retweet"></i>
+                                </a>
+                            </span>
+                        </footer>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card">
+                        <div class="card-content">
+                            <h2 class="title">"Quote"</h2>
+
+                            <h3 class="subtitle">Programmer</h3>
+                        </div>
+
+                        <footer class="card-footer">
+                            <span class="card-footer-item">
+                                <a
+                                    href="#"
+                                    class="button is-success"
+                                >
+                                    <i class="fa fa-thumbs-o-up"></i>
+                                </a>
+                            </span>
+
+                            <span class="card-footer-item">
+                                <a
+                                    href="#"
+                                    class="button is-danger"
+                                >
+                                    <i class="fa fa-thumbs-o-down"></i>
+                                </a>
+                            </span>
+
+                            <span class="card-footer-item">
+                                <a
+                                    href="#"
+                                    class="button is-info"
+                                >
+                                    <i class="fa fa-retweet"></i>
+                                </a>
+                            </span>
+                        </footer>
+                    </div>
+                </div>
+
+                <div class="column">
+                    <div class="card">
+                        <div class="card-content">
+                            <h2 class="title">"Quote"</h2>
+
+                            <h3 class="subtitle">Programmer</h3>
+                        </div>
+
+                        <footer class="card-footer">
+                            <span class="card-footer-item">
+                                <a
+                                    href="#"
+                                    class="button is-success"
+                                >
+                                    <i class="fa fa-thumbs-o-up"></i>
+                                </a>
+                            </span>
+
+                            <span class="card-footer-item">
+                                <a
+                                    href="#"
+                                    class="button is-danger"
+                                >
+                                    <i class="fa fa-thumbs-o-down"></i>
+                                </a>
+                            </span>
+
+                            <span class="card-footer-item">
+                                <a
+                                    href="#"
+                                    class="button is-info"
+                                >
+                                    <i class="fa fa-retweet"></i>
+                                </a>
+                            </span>
+                        </footer>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
